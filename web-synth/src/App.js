94ce742682714be02d8619c0443838ba26713a17
Lydia Morneault <lydia.morneault@gmail.com>
2022-09-30 09:35:13 -0400

    add play function

--- web-synth/src/App.js
+++ web-synth/src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+
+window.AudioContext = window.AudioContext || window.webkitAudioContext;
+var context = new AudioContext();
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <div>OSCILLOSCOPE</div>
+      <div>SINE</div>
+      <div>TRIANGLE</div>
+      <div>SQUARE</div>
+      <div>FREQUENCY</div>
+      <div>VOLUME</div>
+      <div>START/STOP</div>
+      <button onClick={play("sine", 220)}>Sine</button>
+      <button onClick={play("triangle", 277)}>Triangle</button>
       </header>
     </div>
   );
+}
+
+
+function play(wave, freq){
+  var myOscillator = context.createOscillator();
+  myOscillator.frequency.value = freq;
+  myOscillator.type = wave;
+
+  myOscillator.connect(context.destination);
+  myOscillator.start();
+  myOscillator.stop(context.currentTime + 2);
 }
 
 export default App;
