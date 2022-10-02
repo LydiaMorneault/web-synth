6f717ab679bdab78b6e2e698e6f6109463961fad
Lydia Morneault <lydia.morneault@gmail.com>
2022-10-02 10:43:38 -0400

    Create icon buttons

--- web-synth/src/App.js
+++ web-synth/src/App.js
@@ -1,40 +1,32 @@
-import './App.css';
-
-window.AudioContext = window.AudioContext || window.webkitAudioContext;
-var context = new AudioContext();
-
-
-function App() {
+import {play, setFreq, start, stop, setVol} from './audio';
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      <div>OSCILLOSCOPE</div>
-      <div>SINE</div>
-      <div>TRIANGLE</div>
-      <div>SQUARE</div>
-      <div>FREQUENCY</div>
-      <div>VOLUME</div>
-      <div>START/STOP</div>
-      </header>
-      
+      <head>
+        <h1>Header</h1>
+      </head>
       <body>
-        <button>
-          <image src="./sine.jpg" onClick={play("sine", 220)} alt="Sine"/>
-        </button>
-      </body>
+            <image src="sine.jpg" onClick={play('sine')} alt="Sine" />
+            <image src="sawtooth.jpg" onClick={play('sawtooth')} alt="Sine" />
+            <image src="triangle.jpg" onClick={play('square')} alt="Sine" />
+            <image src="square.jpg" onClick={play('triangle')} alt="Sine" />
+            <br/>
+            <button onclick={start()}>On</button>
+            <button onclick={stop()}>Off</button>
+            <br/>
+            <label>
+                Frequency
+                <input type="range" min="20" max="1000" value="220" id="freqRange" step="1" oninput={setFreq()} class="center"/>
+            </label>
+            <label id="freqLabel">220</label>
+            <br/>
+            <label>
+                Volume
+                <input type="range" min="0.01" max="1.0" value="0.5" id="volRange" step="0.01" oninput={setVol()} class="center"/>
+            </label>
+            <label id="volLabel">0.5</label>
+          </body>
     </div>
   );
 }
 
-
-function play(wave, freq){
-  var myOscillator = context.createOscillator();
-  myOscillator.frequency.value = freq;
-  myOscillator.type = wave;
-
-  myOscillator.connect(context.destination);
-  myOscillator.start();
-  myOscillator.stop(context.currentTime + 2);
-}
-
-export default App;
