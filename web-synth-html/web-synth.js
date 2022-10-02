e5d0fdbd8849e56f43986c277fcb7d55d71ff39c
Lydia Morneault <lydia.morneault@gmail.com>
2022-10-02 01:06:09 -0400

    Add frequency and volume sliders

--- web-synth-html/web-synth.js
+++ web-synth-html/web-synth.js
@@ -1,12 +1,33 @@
 window.AudioContext = window.AudioContext || window.webkitAudioContext;
 var context = new AudioContext();
+var osc = context.createOscillator();
+var vol = context.createGain();
 
-function play(wave, freq){
-    var myOscillator = context.createOscillator();
-    myOscillator.frequency.value = freq;
-    myOscillator.type = wave;
-  
-    myOscillator.connect(context.destination);
-    myOscillator.start();
-    myOscillator.stop(context.currentTime + 2);
+
+function play(wave){
+    osc.type = wave;
   }
+
+function start(){
+  osc.start();
+  osc.connect(context.destination);
+  vol.connect(context.destination);
+  //TODO: ERROR WHEN RESTARTING
+}
+
+function stop(){
+  osc.disconnect();
+  vol.disconnect();
+}
+
+function setFreq(){
+  osc.frequency.value = document.getElementById('freqRange').value;
+  label = document.getElementById('freqLabel');
+  label.innerHTML = osc.frequency.value;
+}
+
+function setVol(){
+  vol.gain.setValueAtTime(document.getElementById('volRange').value, context.currentTime);
+  label = document.getElementById('volLabel');
+  label.innerHTML = document.getElementById('volRange').value;
+}
